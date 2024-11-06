860b64739a986dabd2b822caf681396c6ece58ea
Ethan132006 <ethan.plot@uha.fr>
2024-11-06 11:18:23 +0100

    Init

---
+++ scripte.py
@@ -0,0 +1,11 @@
+jour= float( input(" Donner le jour du mois : "))
+heure= float( input("Donner l'heure d'aujourd'hui"))
+minute= float(input("Donner les minutes de l'heure actuel"))
+print(jour)
+print(heure)
+print(minute)
+
+             
+
+
+
---
+++ tp1exo4.py
@@ -0,0 +1,3 @@
+minute= float(input("Donner les minutes écoulé pendant ce mois"))
+date= minute /1440
+print(date)
